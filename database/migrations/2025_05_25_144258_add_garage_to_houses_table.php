9aeb0b319984527e664df52f80e610a9c893ec5a
Jeyker-Dev <mendozajeyker1178@gmail.com>
2025-05-26 21:08:36 -0400

    Added defaul value to garage migration

--- database/migrations/2025_05_25_144258_add_garage_to_houses_table.php
+++ database/migrations/2025_05_25_144258_add_garage_to_houses_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('houses', function (Blueprint $table) {
-            $table->integer('garage')->after('bath');
+            $table->integer('garage')->default(0)->after('bath');
         });
     }
 
